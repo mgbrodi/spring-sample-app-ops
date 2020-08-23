ba0ad39672b5d7b3a9cf16dfa64179d9f17e1569
Maria Gabriella Brodi <mbrodi@pivotal.io>
2020-08-23 15:25:34 -0400

    fix path and var expansion

--- update-sha.sh
+++ update-sha.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed 's/\(digest: .*\)$/digest: $1/g' overlays/$2/kustomization.yaml >k.yaml
-cat overlays/$2/k.yaml 
-mv overlays/$2/k.yaml overlays/$2/kustomization.yaml
+sed "s/\(digest: .*\)$/digest: $1/g" overlays/$2/kustomization.yaml >k.yaml
+cat k.yaml 
+mv k.yaml overlays/$2/kustomization.yaml
